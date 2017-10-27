b51c4ca8f56235d58ea094f4b649356b9a224025
ncl-mansfield <martin.mansfield@ncl.ac.uk>
2017-10-27 15:02:34 +0100

    Revert "Add files via upload"
    This reverts commit 761c6a437d3b998eaad7919cceaeb3b98245e122.

--- Models/steering_controller/generate_fmu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Argument must be 'source' or 'tool'
-
-wget http://overture.au.dk/into-cps/vdm-tool-wrapper/development/latest/fmu-import-export.jar -O overture-fmu.jar
-
-java -jar overture-fmu.jar -name steering_controller -output ../../FMUs/ -root . -export $1
